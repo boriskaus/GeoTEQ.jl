4287151fa89c0f0ad1f6f1d4ff7e2f6c593131c2
Boris Kaus <kaus@uni-mainz.de>
2025-06-06 23:13:22 +0200

    add example to test

--- test/example.jl
+++ test/example.jl
@@ -1,8 +1,7 @@
-using MaSB
+using GeoTEQ, Test
 
 # Load sample data as ply file (points + normals)
-#data = load_ply("example-data/house_dyke_tree.ply")
-data = load_ply("example-data/test_surface_1.ply")
+data = load_ply("example-data/house_dyke_tree.ply")
 
 # Create data structure
 ma = MASB(data, 10)
@@ -14,35 +13,13 @@ compute_balls!(ma)
 
 # Points @ the mid-surface:
 x = ma.D["ma_coords_in"]
+@test  x[10] ≈ [ -0.542655885219574, 3.6914117336273193, -2.8566672801971436]
+
+# test next dataset
+ply_fname = "example-data/test_surface_1.ply"
+radius_ma = 100
+radius_cov = 50
+x1, Eij_av1, Eij_vec1 = compute_median_surface(ply_fname, radius_ma, radius_cov)
 
 
-
-# extract only part of them (since we clearly have 2 fault planes)
-#x1 = [xl[1] for xl in x];
-#ind = findall(x1.>0);
-
-#xp = x[ind]
-
-# 
-Eij_vec, Cij_vec = plane_covariance_eigen(xp,1000);
-
-Eij_11 = [Eij[1,1] for Eij in Eij_vec];
-Eij_12 = [Eij[1,2] for Eij in Eij_vec];
-Eij_13 = [Eij[1,3] for Eij in Eij_vec];
-Eij_21 = [Eij[2,1] for Eij in Eij_vec];
-Eij_22 = [Eij[2,2] for Eij in Eij_vec];
-Eij_23 = [Eij[2,3] for Eij in Eij_vec];
-Eij_31 = [Eij[3,1] for Eij in Eij_vec];
-Eij_32 = [Eij[3,2] for Eij in Eij_vec];
-Eij_33 = [Eij[3,3] for Eij in Eij_vec];
-
-
-
-
-# The medium surface plane is now:
-# ma.D["ma_coords_in"]
-# 
-
-
-
-
+@test x1[100] ≈ [  -469.15838623046875, 6563.29931640625, -4168.66650390625]
